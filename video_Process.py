6c38f5ff49b8dba020340f2c177e9768dcddfbee
Aryan <aryanbisht9458@gmail.com>
2022-12-05 09:12:40 +0530

    Better UI.

--- video_Process.py
+++ video_Process.py
@@ -6,8 +6,18 @@ from subprocess import call, STDOUT
 import shlex
 from PIL import Image
 import math
+from colorama import init
+from termcolor import cprint
+from pyfiglet import figlet_format
+from rich import print
+from rich.console import Console
+from rich.table import Table
+import os
+import getpass
+from rich.progress import track
 
 temp_folder = "frame_folder"
+console = Console()
 
 
 def split_string(s_str, count=10):
@@ -35,7 +45,6 @@ def createTmp():
 def countFrames(path):
     cap = cv2.VideoCapture(path)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    print("Total frames:- ", length)
     return length
 
 
@@ -83,15 +92,16 @@ def FrameCapture(path, op, password, message=""):
         count += 1
 
     if op == 1:
+        print("[cyan]Please wait....[/cyan]")
         makeVideoFromFrame()
-        # To delete frames
-
-        # images = [img for img in os.listdir("frame_folder") if img.endswith(".png")]
-        # for img in images:
-        #     os.remove(os.path.join("frame_folder", img))
+    # if op == 2:
+    # To delete frames
+    #     images = [img for img in os.listdir("frame_folder") if img.endswith(".png")]
+    #     for img in images:
+    #         os.remove(os.path.join("frame_folder", img))
 
     if op == 2:
-        print("Message is :- ", outputMessage)
+        print("[green]Message is :-\n[bold]%s[/bold][/green]" % outputMessage)
 
 
 def makeVideoFromFrame():
@@ -113,34 +123,29 @@ def makeVideoFromFrame():
 
 
 def main():
-    print("In what you want to hide the data 1 for image and 2 for video Using e-LSB\n")
-    print(">>")
-    choice = int(input())
-
-    if choice == 1:
-        text = "Image"
-    else:
-        text = "Video"
-
+    text = "Video"
     print("Choose one: ")
-    op = int(input("1. Encode\n2. Decode\n>>"))
+    print("[cyan]1. Encode[/cyan]\n[cyan]2. Decode[/cyan]")
+    op = int(input(">> "))
 
     if op == 1:
-        print(f"{text} path (with extension): ")
-        img = input(">>")
+        print(f"[cyan]{text} path (with extension): [/cyan]")
+        img = input(">> ")
 
-        print("Message to be hidden: ")
-        message = input(">>")
+        print("[cyan]Message to be hidden: [/cyan]")
+        message = input(">> ")
         password = ""
 
-        print("Password to encrypt (leave empty if you want no password): ")
-        password = getpass.getpass(">>")
+        print(
+            "[cyan]Password to encrypt (leave empty if you want no password): [/cyan]"
+        )
+        password = getpass.getpass(">> ")
 
         if password != "":
-            print("Re-enter Password: ")
-            confirm_password = getpass.getpass(">>")
+            print("[cyan]Re-enter Password: [/cyan]")
+            confirm_password = getpass.getpass(">> ")
             if password != confirm_password:
-                print("Passwords don't match try again ")
+                print("[red]Passwords don't match try again [/red]")
                 return
 
         cmd = shlex.split(f"ffmpeg -i {img} -q:a 0 -map a sample.mp3")
@@ -174,18 +179,30 @@ def main():
         os.remove("sample.mp3")
 
     elif op == 2:
-        print(f"{text} path (with extension): ")
+        print(f"[cyan]{text} path (with extension):[/cyan] ")
         img = input(">>")
 
-        print("Enter password (leave empty if no password): ")
+        print("[cyan]Enter password (leave empty if no password):[/cyan] ")
         password = getpass.getpass(">>")
         FrameCapture(img, op, password)
 
 
-if __name__ == "__main__":
+# To remove Credit in starting commnet this function only.
+def print_credits():
+    table = Table(show_header=True)
+    table.add_column("Author", style="yellow")
+    table.add_column("Contact", style="yellow")
+    table.add_row("Aryan Bisht", "aryanbisht9458@gmail.com ")
+    console.print(table)
 
+
+if __name__ == "__main__":
+    os.system("cls" if os.name == "nt" else "clear")
+    cprint(figlet_format("STEGANO", font="starwars"), "yellow", attrs=["bold"])
+    print_credits()
+    print()
     print(
-        "VIDEOHIDE allows you to hide texts inside an video. You can also protect these texts with a password using AES-256."
+        "[bold]VIDEOHIDE[/bold] allows you to hide texts inside an video. You can also protect these texts with a password using AES-256."
     )
     print()
     main()
